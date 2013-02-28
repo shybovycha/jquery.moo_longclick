830ed41d9a128b8c6826e4d494bced419665f0dd
Artem Shoobovych <shybovycha@gmail.com>
2013-02-28 18:58:29 +0200

    Initial commit

---
+++ jquery.moo_longclick.js
@@ -0,0 +1,56 @@
+(function($) {
+    /*
+     * Long-tap event handler. Supports touch devices.
+     *
+     * $().longclick(handler, [ duration ]);
+     *
+     */
+    $.fn.longclick = function(handler, duration) {
+        duration = duration || 500;
+
+        function moo_bind(elements, events, event_handler) {
+            $(elements).each(function(element_index, element) {
+                if (!events.join) {
+                    events = events.split(/\s+/);
+                }
+
+                $.each(events, function(index, event_name) {
+                    element.addEventListener(event_name, function(evt) {
+                        evt.preventDefault();
+
+                        return event_handler.bind($(element))(evt);
+                    });
+                });
+            });
+        }
+
+        $(this).each(function(index, element) {
+            moo_bind($(element), 'mousedown touchstart', function(evt) {
+                if ($(element).data('moo.timer')) {
+                    return true;
+                }
+
+                var timer = window.setTimeout(function() {
+                    window.clearTimeout($(element).data('moo.timer'));
+                    $(element).data('moo.timer', null);
+                    handler.bind($(element))(evt);
+                }, duration);
+
+                return $(element).data('moo.timer', timer);
+            });
+
+            moo_bind($(element), [ 'mousemove', 'mouseup', 'mouseout', 'contextmenu', 'touchend', 'touchmove', 'touchcancel' ].join(' '), function(evt) {
+                window.clearTimeout($(element).data('moo.timer'));
+                $(element).data('moo.timer', null);
+                return true;
+            });
+
+            moo_bind($(element), 'click', function(evt) {
+                window.clearTimeout($(element).data('moo.timer'));
+                $(element).data('moo.timer', null);
+
+                return handler.bind($(element))(evt);
+            });
+        });
+    };
+})(jQuery);
